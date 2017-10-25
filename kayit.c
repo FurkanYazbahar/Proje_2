21c1f532dbf2bdf6c11eda54f825b6563ec46997
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2017-10-25 19:31:50 +0300

    Update kayit.c

--- kayit.c
+++ kayit.c
@@ -1,49 +1,57 @@
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 #include <string.h>
+// ön tanımlayıcıları şimdilik es geç
+#define STU struct ogrenci
 
 int satir_say(FILE* dosya)
 {
-    int satir=0;
-    char c;
-    while(!feof(dosya))
-    {
-        c=getc(dosya);
-        if(c=='\n')
-            satir++;
-    }
-    return satir+1; // Dosya sonunda EOF olduğundan son satırı saymaz. O yüzden satir+1 yaptık.
+  int satir=0;
+  char c;
+  while(!feof(dosya))
+  {
+    c=getc(dosya);
+    if(c=='\n')
+      satir++;
+  }
+  // Dosya sonunda EOF olduğundan son satırı saymaz. O yüzden satir+1 yaptık.
+  return satir+1; 
+}
+// bir satırın eleman satısını döner
+// cursoru alt satıra geçirmek için kullandık
+int chrinline(FILE* file)
+{
+  int chr_count = 0;
+  while(fgetc(file) != '\n')
+    chr_count++;
+  return chr_count;
 }
 struct ogrenci
 {
-    char ad[15];
-    char soyad[15];
-    char k_sirasi[4];
+    char ad[30];
+    char soyad[30];
+    int  k_sirasi;
     char num[8];
-    char ogretim_tur[4];
+    char ogretim_tur[2];
 };
-/*struct ogrenci al_dosyadan(struct ogrenci* kayit, int boy){
-    struct ogrenci* olustur = (struct ogrenci*)malloc(boy*sizeof(struct ogrenci));
-
-}*/
-int main ()
-{
-    FILE * dosya;
-    dosya = fopen("cccc.txt","r");
-    fseek(dosya,60,SEEK_SET);   // Dosyanın ikinci satırından başlamak için
-    // int satir = satir_say(dosya);
-    //printf("%d  tane kayit vardir. \n",satir);
-    // struct ogrenci* kayit =(struct ogrenci*)malloc((satir+1)*sizeof(struct ogrenci));
-    struct ogrenci ogr;
-    int i=1;
-    printf("furkan\n");
-    while(!feof(dosya))
-    {
-        fscanf(dosya,"%s%s%s%s%s",ogr.num, ogr.ad, ogr.soyad,
-               ogr.k_sirasi, ogr.ogretim_tur);
-        printf("%s\t%s\t%s\t%s\t%s\n",ogr.num, ogr.ad, ogr.soyad,
-               ogr.k_sirasi, ogr.ogretim_tur);
-
-    }
-    return 0;
+int main () {
+  FILE *dosya, *file;
+  dosya = fopen("ogrenci.txt","r");
+  file = fopen("re-writen.txt","w");
+  printf("%d\n", chrinline(dosya));
+  // WTF!
+  // fseek(dosya,60,SEEK_SET);   // Dosyanın ikinci satırından başlamak için
+  STU ogr;
+  
+  printf("furkan\n");
+  while(!feof(dosya))
+  {
+    fscanf(dosya,"%s\t%s\t%s\t%d\t%s\n",ogr.num, ogr.ad, ogr.soyad,
+      &ogr.k_sirasi, ogr.ogretim_tur);
+    fprintf(file, "%s\t\t%s\t\t%s\t\t%d\t\t%s\n",ogr.num, ogr.ad, ogr.soyad,
+      ogr.k_sirasi, ogr.ogretim_tur);
+  }
+  fclose(dosya);
+  fclose(file);
+  return 0;
 }
