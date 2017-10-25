8a7917c3cc438c62bacb1f4ec51b363afb975cab
FurkanYazbahar <32523353+FurkanYazbahar@users.noreply.github.com>
2017-10-25 16:28:38 +0300

    Update kayit.c

--- kayit.c
+++ kayit.c
@@ -1,5 +1,6 @@
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 
 int satir_say(FILE* dosya){
     int satir=0;
@@ -12,26 +13,34 @@ int satir_say(FILE* dosya){
     return satir+1; // Dosya sonunda EOF olduğundan son satırı saymaz. O yüzden satir+1 yaptık.
 }
 struct ogrenci{
-    char numara[7];
     char ad[15];
     char soyad[15];
-    int k_sirasi;
-    char ogretim_tur[2];
+    char k_sirasi[4];
+    char num[8];
+    char ogretim_tur[3];
  };
+/*struct ogrenci al_dosyadan(struct ogrenci* kayit, int boy){
+    struct ogrenci* olustur = (struct ogrenci*)malloc(boy*sizeof(struct ogrenci));
+
+}*/
 int main ()
 {
-    clock_t t;
-    t = clock();
     FILE * dosya;
-    dosya = fopen("ogrenci_kayit_bilgileri.txt","r");
-    fseek(dosya,44,SEEK_SET);
-    printf("satir sayisi : %d",satir_say(dosya));
-    /*do
-    {
-        c = getc(dosya);    // Bir karakter oku
-        putchar(c);         // ekranda goster
-    }while (c != EOF);*/
-    t = clock() - t;
-    printf("\n %f ", ((double)t)/CLOCKS_PER_SEC);
+    dosya = fopen("cccc.txt","r");
+    char c;
+    fseek(dosya,60,SEEK_SET);   // Dosyanın ikinci satırından başlamak için
+    int satir = satir_say(dosya);
+    printf("%d  tane kayit vardir. \n",satir);
+    // struct ogrenci* kayit =(struct ogrenci*)malloc((satir+1)*sizeof(struct ogrenci));
+    struct ogrenci ogr;
+    int i=1;
+    while(!feof(dosya)){
+        fscanf(dosya,"%s%s%s%s%s",&ogr.num, &ogr.ad, &ogr.soyad,
+                &ogr.k_sirasi, &ogr.ogretim_tur);
+        printf("Birinci Kayit %d:\n%s\t%s\t%s\t%s\t%s",i+1, ogr.num, ogr.ad, ogr.soyad,
+                ogr.k_sirasi, ogr.ogretim_tur);
+        i++;
+    }
     return 0;
 }
+
